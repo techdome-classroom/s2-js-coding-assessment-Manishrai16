fa1a85bdd9d04f3813119c29aa2c61af81d27ecf
Manishrai <mrai97641@gmail.com>
2024-09-30 14:09:09 +0530

    done

--- program1.js
+++ program1.js
@@ -3,7 +3,20 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const bracketMap = { ')': '(', '}': '{', ']': '[' };
+
+    for (let char of s) {
+        if (bracketMap[char] === undefined) {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || stack.pop() !== bracketMap[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
